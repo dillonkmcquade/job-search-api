1c22138d1a47b94c85b046552902f55c0ab5b82d
dillonkmcquade <dillonkmcquade@gmail.com>
2020-01-29 14:29:27 -0500

    added express sslify

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const fetch = require("node-fetch");
-const enforce = require('express-sslify');
+const enforce = require("express-sslify");
 
 app.use(cors());
 app.use(express.json());
-app.use(enforce.HTTPS({trustProtoHeader: true}))
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-app.get('/', (req,res) => {
-  res.send('its Working')
+app.get("/", (req, res) => {
+  res.send("its Working");
 });
 
 app.post("/jobs", (req, res) => {
@@ -22,12 +22,12 @@ app.post("/jobs", (req, res) => {
 });
 
 app.get("/initialJobs", (req, res) => {
-  fetch(
-    'https://jobs.github.com/positions.json?description=javascript&page=1'
-  )
+  fetch("https://jobs.github.com/positions.json?description=javascript&page=1")
     .then(response => response.json())
     .then(jobs => res.send(jobs))
     .catch(err => res.status(400).json("ERRORRRRR!!!!"));
 });
 
-app.listen(process.env.PORT || 3001 , () => console.log(`Listening on port ${process.env.PORT} !`));
+app.listen(process.env.PORT || 3001, () =>
+  console.log(`Listening on port ${process.env.PORT} !`)
+);
